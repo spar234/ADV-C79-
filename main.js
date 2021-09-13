2252ad339591fb26070e70deeed2bd8982c48551
spar234 <87600450+spar234@users.noreply.github.com>
2021-09-13 09:51:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+menu_list_array = menu_list_array = ["Veg Margherita Pizza",
+"Chicken Tandoori Pizza",
+"Veg Supreme Pizza",
+"Panner Tikka Pizza",
+"Deluxe Veggie Pizza",
+"Veg Extravaganza Pizza",];
+
+function getmenu(){
+var htmldata;
+htmldata="ol class='menulist'>"
+menu_list_array.sort();
+for(var i=0;i<menu_list_array.lenght;i++){
+    htmldata=htmldata+ '<li>' + menu_list_array[i] + '</li>'
+}
+htmldata=htmldata+"</ol>"
+document.getElementById("display_menu").innerHTM = htmldat;
+var item=document.getElementById("add_item").value;
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+for(var i=0;i<menu_list_array.length;i++);
+
+}
+
+function add_top(){
+    var item=document.getElementById("add_iteam").value;
+    menu_list_array.push(item)
+    add_item();
+}
